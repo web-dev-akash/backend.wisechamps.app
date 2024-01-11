72d7c8323c53e2b17b3efb318d85338e22fe7e90
web-dev-akash <akumar415129.ak@gmail.com>
2024-01-11 17:59:12 +0530

    Final Version for Teachers Reports

--- index.js
+++ index.js
@@ -2049,7 +2049,7 @@ const getDailyReports = async (grade, team) => {
       Authorization: `Bearer ${zohoToken}`,
     },
   };
-  const date = new Date("2024-01-06");
+  const date = new Date();
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
